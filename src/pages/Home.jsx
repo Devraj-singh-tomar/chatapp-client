49840f4f42f8f7eb71331924297ec5a85b2e2694
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2025-03-11 12:43:10 +0530

    feat_UI: update Home component to display a friend selection prompt with improved styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import AppLayout from "../components/layout/AppLayout";
+import { Box, Typography } from "@mui/material";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <Box height={"100%"}>
+      <Typography pt={"2rem"} color="white" variant="h5" textAlign={"center"}>
+        Select a friend to chat
+      </Typography>
+    </Box>
+  );
 };
 
 export default AppLayout()(Home);
